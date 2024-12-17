f8c62ddbc52102dfedf688404472b2042a30178f
Marie-anne <marianamaganga339@gmail.com>
2024-12-17 10:18:06 +0300

    refactor: update data source configuration to disable auto-sync in production

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,14 +4,14 @@ import { Category } from './categories/entities/categories.entity';
 import { Transaction } from './transactions/entities/transactions.entity';
 
 export const dataSource = new DataSource({
-  type: 'oracle',  // Specify Oracle as the database type
+  type: 'oracle', 
   host: 'localhost',
-  port: 1521,  // Default Oracle port
+  port: 1521, 
   username: 'SYSTEM',
   password: 'password',
-  sid: 'FREE',  // Oracle SID or Service Name
-  entities: [User, Category, Transaction],  // Your entity classes
-  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],  // Path to migrations
-  synchronize: true,  // Set to false to avoid auto-sync in production
+  sid: 'FREE',  
+  entities: [User, Category, Transaction],  
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],  
+  synchronize: false, 
   logging: true,
 });
