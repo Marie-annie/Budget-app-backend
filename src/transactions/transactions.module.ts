d6bb8575952311da8cfa75f922908c340a06f8aa
Marie-anne <marianamaganga339@gmail.com>
2024-12-17 10:17:47 +0300

    refactor: add User and Category entities to Transactions module imports

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { Transaction } from './entities/transactions.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Category } from 'src/categories/entities/categories.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [TypeOrmModule.forFeature([Transaction, User, Category])],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
